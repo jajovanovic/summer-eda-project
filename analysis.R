041b04cbc8cfb8dd21e724c1d029f8898eceb016
Jackie Jovanovic <jjovanovic@elon.edu>
2022-07-07 14:09:56 -0400

    update

--- analysis.R
+++ analysis.R
@@ -26,15 +26,19 @@ names(Batting)
 # shifts ------------------------------------------------------------------
 
 if_shift <- Batting %>% 
+  filter(!is.na(if_fielding_alignment)) %>% 
   ggplot(aes(x = if_fielding_alignment)) +
   geom_bar() +
   labs(title = "Infield Shift",
-       x = "fielding alignment")
+       x = "fielding alignment") +
+  theme_bw()
 of_shift <- Batting %>% 
+  filter(!is.na(of_fielding_alignment)) %>% 
   ggplot(aes(x = of_fielding_alignment)) +
   geom_bar() +
   labs(title = "Outfield Shift",
-       x = "fielding alignment")
+       x = "fielding alignment") +
+  theme_bw()
 
 if_shift + of_shift
 
@@ -43,10 +47,42 @@ if_shift + of_shift
 
 Batting %>% 
   ggplot(aes(x = launch_angle, y = launch_speed)) +
-  geom_point(alpha = 0.3) +
+  geom_point(alpha = 0.25) +
   geom_density2d() +
   coord_flip() +
-  theme_bw()
+  theme_bw() +
+  labs(title = "Launch Angle vs. Launch Speed", 
+       x = "Launch Speed",
+       y = "Launch Angle")
+Batting %>% 
+  ggplot(aes(x = launch_angle, y = launch_speed)) +
+  stat_density2d(geom = "polygon", # use stat not geom
+                 #contour = FALSE,
+                 adjust = .75,
+                 aes(fill = after_stat(level))) + 
+  scale_fill_gradient(low = "darkblue", high = "darkorange",
+                      labels = scales::comma) +
+  theme_bw() +
+  #theme(legend.position = "bottom") +
+  coord_equal() +
+  labs(title = "Launch Angle vs. Launch Speed", 
+       x = "Launch Angle",
+       y = "Launch Speed") 
+
+delta_score %>% 
+  ggplot(aes(factor(scored_rbi), launch_angle, fill = scored_rbi)) +
+  scale_fill_viridis_b() +
+  geom_boxplot() +
+  theme_bw() +
+  labs(x = "Runs Scored", y = "Launch Angle") +
+  theme(legend.position = "none")
+delta_score %>% 
+  ggplot(aes(factor(scored_rbi), launch_speed, fill = scored_rbi)) +
+  scale_fill_viridis_b() +
+  geom_boxplot() +
+  theme_bw() +
+  labs(x = "Runs Scored", y = "Launch Speed") +
+  theme(legend.position = "none")
 
 
 # hit distance by batted ball type ----------------------------------------
@@ -61,6 +97,14 @@ Batting %>%
 Batting %>% 
   ggplot(aes(x = hc_x, y = hc_y, color = pitch_type)) +
   geom_point(alpha = 0.3)
+edit_pitch %>% 
+  ggplot(aes(x = hc_x, y = (hc_y * -1), color = pitch_type)) +
+  geom_point(alpha = 0.3) +
+  labs(x = "horizontal coordinate",
+       y = "vertical coordinate",
+       color = "Pitch Type") +
+  facet_wrap(~ stand) +
+  theme_bw()
 
 
 # pitch type --------------------------------------------------------------
@@ -94,6 +138,12 @@ edit_pitch %>%
   labs(title = "Balls") +
   theme_bw() +
   theme(legend.position = "bottom") 
+
+# table of probability of each pitch type overall
+edit_pitch %>% 
+  group_by(pitch_type) %>% 
+  summarise(n = n()) %>%
+  mutate(prop = n / sum(n)) 
 
 
 # table of pitch types overall --------------------------------------------
@@ -294,7 +344,7 @@ counts2 <- c("0-0", "0-1", "0-2", "1-0", "1-1", "1-2", "2-0", "2-1", "2-2",
 changeup_props %>% 
   ggplot(aes(x = count, y = prop)) +
   theme_bw() +
-  geom_col() +
+  geom_col(fill= "#238A8DFF") +
   labs(title = "Changeup") +
   scale_x_discrete(limits = counts2)
 
@@ -302,17 +352,26 @@ breaking_props %>%
   ggplot(aes(x = count, y = prop)) +
   theme_bw() +
   geom_col() +
-  labs(title = "Breaking balls") +
+  geom_col(fill= "#238A8DFF") +
+  labs(title = "Breaking balls",
+       y = "proportion") +
   scale_x_discrete(limits = counts2)
 
 fast_props %>% 
   ggplot(aes(x = count, y = prop)) +
   theme_bw() +
   geom_col() +
+  geom_col(fill= "#238A8DFF") +
   labs(title = "Fastball") +
   scale_x_discrete(limits = counts)
 
+max(changeup_props$prop) - min(changeup_props$prop) # 0.1217507
+max(breaking_props$prop) - min(breaking_props$prop) # 0.3158582
+max(fast_props$prop) - min(fast_props$prop) # 0.5477707
 
+summary(changeup_props$prop)
+summary(breaking_props$prop)
+summary(fast_props$prop)
 
 # runs scored -------------------------------------------------------------
 
@@ -325,6 +384,88 @@ scoring %>%
          effective_speed) %>% 
   summary()
 
+Batting %>% 
+  filter(post_home_score > home_score) %>% 
+  ggplot(aes(x = hit_distance_sc, y = (post_home_score - home_score))) +
+  geom_line() +
+  coord_flip()
+
+
+# scoring -----------------------------------------------------------------
+#### NEW NEW NEW NEW ####
+
+# new var
+
+delta_home <- Batting %>% # ignore
+  filter(post_home_score > home_score) %>% 
+  mutate(runs_scored = post_home_score - home_score)
+delta_score <- Batting %>% # ignore
+  if(post_home_score > home_score){
+    mutate(runs_scored = post_home_score - home_score)
+  } else if(post_away_score > away_score){
+    mutate(runs_scored = post_away_score - away_score)
+  } else{
+    mutate(runs_scored == 0)
+  }
+delta_score <- Batting %>% # ignore
+  if(post_home_score >= home_score){
+    mutate(runs_scored = post_home_score - home_score)} %>% 
+  if(post_away_score > away_score){
+    mutate(runs_scored = post_away_score - away_score)} 
+  #if(post_home_score > home_score | post_away_score > away_score){
+   # mutate(runs_scored == 0)
+  #}
+delta_score <- Batting %>% 
+  mutate(scored_rbi = case_when(post_home_score != home_score ~ 
+                                  post_home_score - home_score,
+                                post_away_score != away_score ~ 
+                                  post_away_score - away_score,
+                                TRUE ~ as.numeric(0)))
+delta_score$scored_rbi
+
+delta_score %>% 
+  ggplot(aes(x = launch_speed, y = hit_distance_sc, size = scored_rbi,
+             color = effective_speed)) +
+  geom_point(alpha = 0.3) +
+  scale_color_gradient(low = "darkviolet", high = "darkturquoise") +
+  labs(title = "Hit Data",
+       x = "Launch Speed", y = "Hit Distance",
+       color = "Effective Speed", size = "Runs Scored") +
+  theme_bw() +
+  theme(legend.position = "bottom")
+delta_score %>% 
+  ggplot(aes(x = hit_distance_sc, y = launch_angle, color = scored_rbi,
+             size = scored_rbi)) +
+  geom_point(alpha = 0.3) +
+  scale_color_gradient(low = "steelblue3", high = "darkviolet") +
+  theme_bw() +
+  labs(x = "Hit Distance", y = "Launch Angle",
+       color = "Runs Scored", size = "Runs Scored") +
+  theme(legend.position = "bottom")
+#### NEW NEW NEW NEW ####
+
+# pitch type
+delta_score2 <- delta_score
+delta_score2 <- delta_score2 %>% 
+  filter(!is.na(pitch_type)) %>% 
+  mutate(pitch_type = fct_recode(pitch_type, "Changeup" = "CH", 
+                                 "Breaking ball" = "CU",
+                                 "Changeup" = "EP",
+                                 "Fastball" = "FA",
+                                 "Fastball" = "FC", 
+                                 "Fastball" = "FF", 
+                                 "Fastball" = "FS",
+                                 "Breaking ball" = "KC",  
+                                 "Fastball" = "SI",  
+                                 "Breaking ball" = "SL"))
+delta_score2 %>% 
+  select(pitch_type, scored_rbi) %>% 
+  group_by(pitch_type, scored_rbi) %>% 
+  summarise(n = n()) %>%
+  mutate(prop = n / sum(n)) %>% 
+  ggplot(aes(x = scored_rbi, y = prop)) +
+  geom_col() +
+  facet_grid(scored_rbi ~ pitch_type)
 
 # counts of pitch types by number of balls
 edit_pitch %>% 
